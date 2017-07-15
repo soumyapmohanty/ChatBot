a2b67aa29ac2f2f6baf9c62de188e136d63d3743
Mohanty <Mohanty@DESKTOP-EUL8QUR>
2017-07-15 13:15:59 +0530

    Added files

---
+++ ChatBot/Index.js
@@ -0,0 +1,25 @@
+﻿'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+app.set('port', (process.env.PORT || 5858))
+//Allow to process the data
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json)
+// register routes
+app.get('/', function (req, res) {
+res.send("hi this my first bot!!!")
+})
+// connect to facebook messenger
+
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === "soumyap.mohanty")
+	{
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong Token!!")
+})
+app.listen(app.get('port'), function () {
+	console.log("running on port 5858")
+})
